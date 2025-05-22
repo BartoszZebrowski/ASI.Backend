b1d712873852118eb2323c8b8a8dbf0548b4f85b
BartoszZebrowski <62619839+BartoszZebrowski@users.noreply.github.com>
2025-05-22 21:53:53 +0200

    Zrobione usuwanie kolumn

--- asibackend/src/asibackend/pipelines/data_processing/nodes.py
+++ asibackend/src/asibackend/pipelines/data_processing/nodes.py
@@ -7,5 +7,34 @@ generated using Kedro 0.19.13
 
 
 def drop_columns(df: pd.DataFrame) -> pd.DataFrame:
-    # np.
-    return df.drop(columns=["do_usuniecia"])
+
+    df = df.drop(columns=[
+        "Employee_Name",
+        "EmpID",
+        "MarriedID",
+        "PositionID",
+        "MaritalStatusID",
+        "LastPerformanceReview_Date",
+        "RecruitmentSource",
+        "ManagerName",
+        "DateofTermination",
+        "RaceDesc",
+        "CitizenDesc",
+        "MaritalDesc",
+        "HispanicLatino",
+        "Sex",
+        "DOB",
+        "Zip",
+        "State",
+        "Termd",
+        "FromDiversityJobFairID",
+        "PerfScoreID",
+        "EmpStatusID",
+        "GenderID",
+        "DeptID",
+        "ManagerID",
+        "SpecialProjectsCount"
+    ], 
+    errors="ignore")
+
+    return df
